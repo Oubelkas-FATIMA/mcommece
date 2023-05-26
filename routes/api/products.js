25c59b3af8957416d318eb62a985ff00d5f6a08d
Oubelkas-FATIMA <oubalkassf@gmail.com>
2023-05-26 20:41:48 +0100

    First commit

---
+++ routes/api/products.js
@@ -0,0 +1,24 @@
+const  express = require('express');
+const  router =  express.Router();
+
+const Product = require('../../models/Product');
+
+// @route GET api/products
+// @desc  GET all products
+// @access Public
+router.get('/', (req, res) => {
+    Product.find()
+        .then( products => res.json(products))
+});
+
+// @route GET api/products
+// @desc  GET a product
+// @access Public
+router.get('/:id', (req, res) => {
+    Product.findById(req.params.id)
+        .then( product => res.json(product))
+        .catch( err => res.status(404).json({error: "product not found"}))
+
+});
+
+module.exports =  router;
